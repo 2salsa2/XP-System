7c062e6fb12c37d754729b5afbb1aa755b7d1999
2salsa2 <sksky217@gmail.com>
2020-03-25 18:18:44 -0400

    importing json and changing directory

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 import os
+import json
 from dotenv import load_dotenv
-
 import discord
 from discord.ext import commands
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+git config --global user.email "sksky217@gmail.com"
+git config --global user.name "2salsa2"
 
+load_dotenv()
+TOKEN = os.getenv('Njg2NzI3ODAzNjQyNTc2OTQx.XnvNHg.xhtb8tm5uRvcJrZyHoUx-SOTX28')
 bot = commands.Bot(command_prefix='!')
+os.chdir(r"C:\Users\khali\github\XP-System")
 
 @bot.listen()
 async def on_ready():
@@ -17,6 +20,13 @@ async def on_ready():
 async def say_hello(ctx):
     await ctx.send(f"Hi {ctx.author.display_name}")
 
+
+@clinet.event
+async def status (member):
+    with open('discord.members.json',"r") as j:
+        members = json.load(j)
+
+        
+
+
 bot.run(TOKEN)
-
-
