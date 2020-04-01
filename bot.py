2eb86ec65336aaa9ef5489551eef6edd2d23cf66
2salsa2 <sksky217@gmail.com>
2020-04-01 19:03:48 -0400

    bug fixes 3

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ async def say_hello(ctx):
     await ctx.send(f"Hi {ctx.author.display_name}")
 
 
-@bot.event(name="status", help="loads users")
+@bot.listen(name="status", help="loads users")
 async def status (member):
     with open('discord.members.json',"r") as j:
         users = json.load(j)
@@ -27,7 +27,7 @@ async def status (member):
         with open("discord.member.json", "d") as j:
             json.dump(users, j)
 
-@bot.event(name="message players", help="messages players amount on xp into discord channel")
+@bot.listen(name="message players", help="messages players amount on xp into discord channel")
 async def message_p(message):
     with open ("discord.member.json", "r") as j:
         users = json.load(j)
@@ -37,25 +37,25 @@ async def message_p(message):
         with open("discord.members.json", "d") as j:
             json.dump(users, j)
 
-@bot.event(name="update", help="updates json file")
+
 async def update_me(users, user):
     if not user.id in users:
         users[user.id] = {}
         users[user.id]['exp'] = 0
         users[user.id]['level'] = 1
 
-@bot.event(name="adds_exp", help="adds xp to player data")
+
 async def plus_xp(users, user, xp):
     users[user.id]['exp'] += xp
 
-@bot.event(name="level_up", help="everytime you add exp it makes it harder for one to level up")
+
 async def level_up(users, user, channel):
     exp = users[user.id]["exp"]
     start = users[user.id]['level']
     end = int(exp ** (1/8))
 
     if start < end:
-        await client.send_message(channel, "{} has level up congrats you are level {}".format(user.mention, end))
+        await bot.send_message(channel, "{} has level up congrats you are level {}".format(user.mention, end))
         users[user.id]['level'] = end
 
 
